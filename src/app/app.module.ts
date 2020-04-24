b3fea4c118f029ea6eb5742e75126f6e6c7abfbc
Sébastien Marchand <sebastien.marchand78@gmail.com>
2020-04-24 10:29:26 +0200

    1e version TP1 Angular Premier composant Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BonjourComponent } from './bonjour/bonjour.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BonjourComponent
   ],
   imports: [
     BrowserModule,
